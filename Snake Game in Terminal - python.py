f4d5818fd7ad60900f3c5db732059ef367b73bed
Eugen Gheorghe <eugengheorghe.med@gmail.com>
2025-07-20 21:41:31 +0300

    Update Snake Game in Terminal - python.py
    I added a nice banner at the top, level, score and tweaked a few things to make the game run smoother.

--- Snake Game in Terminal - python.py
+++ Snake Game in Terminal - python.py
@@ -5,8 +5,8 @@ os.system("")
 #Timer created for game refresh rate
 def timer(t):
     while True:
-        for n in range(t*1000000):
-            if n == t*500000: return "A"
+        for n in range(t*150000):
+            if n == t*125000: return "A"
 
 #----------------------------------------------------------------------------
 #Creates playable area
@@ -32,13 +32,15 @@ down = length+2       # Goes Down
 path_left, path_right, path_up, path_down = True, False, False, False
 snake = [113,114,115,116,117,118]
 snack = random.randint(20,(height*length)-50)
-
-#----------------------------------------------------------------------------
-#Runs game in a forever loop
-while game:
+lf_rt_speed = 30                    # Left/right moving speed
+up_dn_speed = int(lf_rt_speed*1.6)  # Up or down moving speed (slower)
+score = 0
+level = 1
 
 #----------------------------------------------------------------------------
 #Gets user input (left,right,up,down), moves head of snake, saves inp in path
+def get_user_input():
+    global snake,path_left,path_right,path_up,path_down
     if keyboard.is_pressed('left') :
         snake.insert(0,snake[0]+left)
         snake.pop(-1)
@@ -69,32 +71,49 @@ while game:
         path_down = True
 
 #----------------------------------------------------------------------------
+#Runs game in a forever loop
+while game:
+    get_user_input()
+
+#----------------------------------------------------------------------------
 #Continues snake movement from last user input saved in path
     if path_left:
         snake.insert(0,snake[0]+left)
         snake.pop(-1)
+        mvmt = lf_rt_speed
     elif path_right:
         snake.insert(0,snake[0]+right)
         snake.pop(-1)
+        mvmt = lf_rt_speed
     elif path_up:
         snake.insert(0,snake[0]+up)
         snake.pop(-1)
+        mvmt = up_dn_speed
     elif path_down:
         snake.insert(0,snake[0]+down)
         snake.pop(-1)
+        mvmt = up_dn_speed
 
 #----------------------------------------------------------------------------
 #Refresh rate
-    if timer(20) == 'A':
+    if timer(mvmt) == 'A':
+
+#----------------------------------------------------------------------------
+#Gets input again to reduce lag caused by slow refresh rate (~0.5sec)
+        get_user_input()
+
+#----------------------------------------------------------------------------
+#Refresh rate again to slow down snake speed in order to be playable
+        if timer(mvmt) == 'A':
 
 #----------------------------------------------------------------------------
 #Positions the snake on the board and checks for collisions with edges
-        for pos in snake:
-            if board[pos] in ('\u2550','\u2551','\u2554','\u2557','\u255A','\u255D'):
-                board[pos] = "X"
-                game = False
-            else:
-                board[pos] = "+"
+            for pos in snake:
+                if board[pos] in ('\u2550','\u2551','\u2554','\u2557','\u255A','\u255D'):
+                    board[pos] = "X"
+                    game = False
+                else:
+                    board[pos] = "+"
 
 #----------------------------------------------------------------------------
 #Adds random snacks to board and to snake if eaten
@@ -103,18 +122,44 @@ while game:
     else:
         snack = random.randint(20,200)
         snake.append(snake[-1])
+        score += 1
 
 #----------------------------------------------------------------------------
 #Prints the update board and clears board (ready for next update)
-    print("\n"+" "*int(((length+2)/2)-3)+"SNAKE!"+" "*int(((length+2)/2)-3)+"\n")
+    print(f"\n{' '*36}▒█▀▀▀█ █▀▀▄ █▀▀█ █░█ █▀▀ ")
+    print(f"{' '*36}░▀▀▀▄▄ █░░█ █▄▄█ █▀▄ █▀▀ ")
+    print(f"{' '*36}▒█▄▄▄█ ▀░░▀ ▀░░▀ ▀░▀ ▀▀▀\n")                                                   
+    print(f"{' '*24}Use ↑ ↓ ← → on your keyboard to control the Snake")
+    print(f"{' '*22}Don't touch the edges, be carefull with your movement")
+    print(f"{' '*23}Try to eat as many random snacks ('O') as possible")
+    print(f"{' '*24}As the Snake gets bigger the speed will increase")
+    print(f"{' '*44}Have fun!")
+    print(f"{' '*24}Level: {level}{' '*32}Score: {score}")
     for t in range(0, len(board), length+2):
-        print("".join(board[t:t+length+2]))
-    print(end=f"\033[{height+3}A\r")
+        print(f"{' '*32}{"".join(board[t:t+length+2])}")
+    print(end=f"\033[{height+11}A\r")
     board = refresh_board.copy()
 
 #----------------------------------------------------------------------------
-#Clears terminal if snake collides with edge
-else:
-    if timer(100) == 'A': 
-        os.system('cls' if os.name == 'nt' else 'clear')
+#Increases speed (level) if snake gets bigger 
+    if len(snake) >= 10:
+        lf_rt_speed = 20
+        up_dn_speed = int(lf_rt_speed*1.6)
+        level = 2
+    if len(snake) >= 15:
+        lf_rt_speed = 10
+        up_dn_speed = int(lf_rt_speed*1.6)
+        level = 3
+    if len(snake) >= 20:
+        lf_rt_speed = 8
+        up_dn_speed = int(lf_rt_speed*1.6)
+        level = 4
+
 #----------------------------------------------------------------------------
+#Prints GAME OVER and clears terminal if snake collides with edge
+else:
+    print(f"{'\n'*(height+11)}")
+    print(f"{' '*30}█▀▀ ▄▀█ █▀▄▀█ █▀▀   █▀█ █░█ █▀▀ █▀█")
+    print(f"{' '*30}█▄█ █▀█ █░▀░█ ██▄   █▄█ ▀▄▀ ██▄ █▀▄")
+    if timer(800) == 'A': 
+        os.system('cls' if os.name == 'nt' else 'clear')
